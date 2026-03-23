2dbd8a3eaf21343b75803e6bb68bea036884a610
Punya <sandhyachamaraju@gmail.com>
2026-03-23 22:49:27 +0530

    Fixed Korean button

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 function generatePlan(){
-    let subject=document.getElementById("subject").Value;
+    let subject=document.getElementById("subject").value;
     let date= new Date (document.getElementById("examDate").value);
     let today=new Date();
     let daysLeft=Math.ceil((date-today)/(1000*60*60*24));
 
 if (subject===""|| daysLeft <=0){
-    document.getElementById("output").innerhtml="Please fill all fields!";
+    document.getElementById("output").innerhtml="Enter valid Details!";
     return;
 }
-let plan=" You have"+daysLeft+"days left.<br>";
+let plan=" You have" + daysLeft + "days left.<br>";
 for(let i=1;i<=daysLeft;i++){
     plan +="Day"+i+":Study"+subject+"<br>";
 }
 document.grtElementById("output").innerhtml=plan;
 }
 function translateToKorean(){
-    document.querySelector("h1").innerText="AI 학습 계획기"
-    alert("Clicked!");
+   
+    document.querySelector("h1").innerText="AI 학습 계획기";
 }
