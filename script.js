331f6e2fbe80ee264d9d225495954dd5928fbb75
Punya <sandhyachamaraju@gmail.com>
2026-03-23 22:16:47 +0530

    Added Korean feature

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 function generatePlan(){
     let subject=document.getElementById("subject").Value;
-    let date=document.getElementById("examDate").value;
-if (subject===""|| date==="")
-{
+    let date= new Date (document.getElementById("examDate").value);
+    let today=new Date();
+    let daysLeft=Math.ceil((date-today)/(1000*60*60*24));
+
+if (subject===""|| daysLeft <=0){
     document.getElementById("output").innerhtml="Please fill all fields!";
     return;
 }
-document.grtElementById("output").innerhtml="Study"+subject+"daily until " +date+"*~*"}
+let plan=" You have"+daysLeft+"days left.<br>";
+for(let i=1;i<=daysLeft;i++){
+    plan +="Day"+i+":Study"+subject+"<br>";
+}
+document.grtElementById("output").innerhtml=plan;
+}
+function translateToKorean(){
+    document.querySelector("h1").innerText=""
+}
